f6fe3da102ffc79eefdbd985c611edb3f0ea7203
kiitoss <antoinechataignier1998@orange.fr>
2021-10-15 21:56:47 +0200

    feat(tlex): add functions for lexicographical table and hash table

--- src/tlex.c
+++ src/tlex.c
@@ -3,6 +3,115 @@
 
 #include "tlex.h"
 
-tlex init_tlex() {
-  return NULL;
+/* Retourne la valeur de hash du lexème*/
+static int hashLexeme(char *lexeme)
+{
+  int i, sommeAscii = 0;
+  for (i = 0; lexeme[i] != '\0'; i++)
+  {
+    sommeAscii += lexeme[i];
+  }
+  return sommeAscii;
+}
+
+/* Retourne 1 les les deux lexèmes sont identiques, 0 sinon */
+static int identiques(char *lexeme1, char *lexeme2)
+{
+  int i;
+  int identiques = 1;
+
+  for (i = 0; lexeme1[i] != '\0'; i++)
+  {
+    if (lexeme1[i] != lexeme2[i])
+    {
+      identiques = 0;
+      break;
+    }
+  }
+
+  return identiques;
+}
+
+/* Retourne la taille du lexème */
+static int tailleLexeme(char *lexeme)
+{
+  int taille;
+  for (taille = 0; lexeme[taille] != '\0'; taille++)
+    ;
+  return taille;
+}
+
+/* Ecrit les informations du lexeme dans la table de hashcode et la table lexicographique */
+static void ecrit(int index, int hashVal, int taille, char *lexeme)
+{
+  thash[index] = hashVal;
+  tlex[index].taille = taille;
+  tlex[index].lexeme = lexeme;
+  tlex[index].suivant = -1;
+}
+
+
+/* initialise la table de hascode */
+void initThash()
+{
+  int i;
+  for (i = 0; i < TLEX_TAILLE_MAX; i++)
+    thash[i] = VALEUR_NULL;
+}
+
+/* Retourne le lexème correspondant au numéro lexicographique */
+char *lexeme(int numLexicographique)
+{
+  if (numLexicographique < 0 || numLexicographique > TLEX_TAILLE_MAX)
+    return NULL;
+  if (tlex[numLexicographique].taille == 0)
+    return NULL;
+
+  return tlex[numLexicographique].lexeme;
+}
+
+/* Affiche la table lexicographique */
+void afficheTableLexico()
+{
+  int i;
+  printf("---------------------------------------------------------------------\n");
+  for (i = 0; i < TLEX_TAILLE_MAX && thash[i] != VALEUR_NULL; i++)
+  {
+    printf("%d\t|\t%d\t|\t%d\t|\t%s\n", i, tlex[i].taille, tlex[i].suivant, tlex[i].lexeme);
+  }
+  printf("---------------------------------------------------------------------\n");
+}
+
+/* Insère le lexeme dans la table de hashcode et la table lexicographique */
+int inserer(char *lexeme)
+{
+  int i, hashVal, taille, returnVal = -1, ancienDernier = -1;
+  hashVal = hashLexeme(lexeme);
+  taille = tailleLexeme(lexeme);
+
+  for (i = 0; i < TLEX_TAILLE_MAX; i++)
+  {
+    if (thash[i] == VALEUR_NULL)
+    {
+      ecrit(i, hashVal, taille, lexeme);
+      if (ancienDernier != -1)
+        tlex[ancienDernier].suivant = i;
+      returnVal = i;
+      break;
+    }
+
+    if (thash[i] == hashVal)
+    {
+      if (identiques(lexeme, tlex[i].lexeme))
+        break;
+
+      if (tlex[i].suivant != VALEUR_NULL)
+      {
+        i = tlex[i].suivant - 1;
+      } else {
+        ancienDernier = i;
+      }
+    }
+  }
+  return returnVal;
 }
--- src/tlex.h
+++ src/tlex.h
@@ -1,9 +1,31 @@
-/* la structure de la table lexicographique */
-struct entry
+#ifndef TLEX_H
+#define TLEX_H
+
+#define TLEX_TAILLE_MAX 500
+#define VALEUR_NULL -1
+
+/* la structure d'une entrée dans la table lexicographique */
+struct entree
 {
-  int size;
+  int taille;
   char *lexeme;
-  struct entry *next;
+  int suivant;
 };
 
-typedef struct entry *tlex;
+struct entree tlex[TLEX_TAILLE_MAX];
+
+int thash[TLEX_TAILLE_MAX];
+
+/* initialise la table de hascode */
+void initThash();
+
+/* Retourne le lexème correspondant au numéro lexicographique */
+char *lexeme(int numLexicographique);
+
+/* Affiche la table lexicographique */
+void afficheTableLexico();
+
+/* Insère le lexeme dans la table de hashcode et la table lexicographique */
+int inserer(char *lexeme);
+
+#endif
